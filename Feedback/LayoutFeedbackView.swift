eea09e1c5f02a0fe10cd68315490af8c24c47107
Ollie Hayman <discorev@olliespage.net>
2015-05-21 23:13:06 +0100

    Refactored slider layout code to the layout controller and general cleaning up of init code for the modern view controller

--- Feedback/LayoutFeedbackView.swift
+++ Feedback/LayoutFeedbackView.swift
@@ -15,6 +15,7 @@ class LayoutFeedbackView {
     
     private let view: UIView
     private let sysModel: feedbackModel
+    private var ioSliders = false
     
     // MARK:- Initalizer methods
     
@@ -22,6 +23,57 @@ class LayoutFeedbackView {
     {
         view = aView
         sysModel = aModel
+    }
+    
+    // MARK:- Layout IO and buttons for default view
+    
+    func layoutIOSliders(#inputSlider: UIVerticalSlider, outputSlider: UIVerticalSlider)
+    {
+        
+        view.addSubview(inputSlider)
+        view.addSubview(outputSlider)
+        // Ok, now we do some magic to make these bad boys look right... and this is before the logic to make the
+        // feedback model get laid out... This "Modern" method seems a lot more cluttered than the old resizing
+        // masks ever did. Oh well... We must do what we must do.
+        view.addConstraint(NSLayoutConstraint(item: inputSlider, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 0.90, constant: 0.0))
+        let constant:CGFloat = -1.0*((0.90*view.bounds.size.height/2)-(inputSlider.intrinsicContentSize().height/2))
+        NSLog("height: %f", constant);
+        view.addConstraint(NSLayoutConstraint(item: inputSlider, attribute: .Leading, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier:1.0, constant: constant))
+        view.addConstraint(NSLayoutConstraint(item: inputSlider, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1.0, constant: 0.0))
+        
+        // Now we need to fix the output slider. Again, we use the same principles as above.
+        view.addConstraint(NSLayoutConstraint(item: outputSlider, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 0.90, constant: 0.0))
+        view.addConstraint(NSLayoutConstraint(item: outputSlider, attribute: .Trailing, relatedBy: .Equal, toItem: view, attribute: .Right, multiplier: 1.0, constant: -1.0*constant))
+        view.addConstraint(NSLayoutConstraint(item: outputSlider, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1.0, constant: 0.0))
+        
+        // mark that the ioSliders have been layed out
+        ioSliders = true
+    }
+    
+    func layoutInfoButton(infoButton: UIButton, rightOf outputSlider: UIVerticalSlider? = nil)
+    {
+        if ioSliders && outputSlider != nil {
+            // Now position the infoButton relative to the slider. Due to the height of the slider really being it's width, we want half this value which is actually the center in the X axis (by auto-layout) so that's used to position the button on top of the slider just off center. Then by using the height (the fixed height of a normal UISlider) the button can be adjusted to sit nicely at the side.
+            view.addConstraint(NSLayoutConstraint(item: infoButton, attribute: .Right, relatedBy: .Equal, toItem: outputSlider, attribute: .CenterX, multiplier: 1.0, constant: -0.75*outputSlider!.bounds.height))
+        } else {
+            view.addConstraint(NSLayoutConstraint(item: infoButton, attribute: .Right, relatedBy: .Equal, toItem: view, attribute: .Right, multiplier: 1.0, constant: -15.0))
+        }
+    }
+    
+    func layoutSelectModelButton(selectModelButton: UIButton, leftOf inputSlider: UIVerticalSlider? = nil)
+    {
+        if ioSliders && inputSlider != nil {
+            view.addConstraint(NSLayoutConstraint(item: selectModelButton, attribute: .Left, relatedBy: .Equal, toItem: inputSlider, attribute: .CenterX, multiplier: 1.0, constant: 0.75*inputSlider!.bounds.height))
+        } else {
+            view.addConstraint(NSLayoutConstraint(item: selectModelButton, attribute: .Left, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1.0, constant: 15.0))
+        }
+    }
+    
+    func layoutInputLabel(inputLabel: UILabel)
+    {
+        if ioSliders {
+            view.addSubview(inputLabel)
+        }
     }
     
 }
--- Feedback/ModernFeedbackViewController.swift
+++ Feedback/ModernFeedbackViewController.swift
@@ -22,6 +22,8 @@ class ModernFeedbackViewController: UIViewController,UIPopoverPresentationContro
     @IBOutlet weak private var selectModelButton: UIButton!
     @IBOutlet weak private var showGraphButton: UIButton!
     
+    // MARK:- Initalization and setup functions
+    
     required init(coder aDecoder: NSCoder)  {
         // setup the input label on init
         inputLabel = UILabel(frame: CGRectMake(31, 57, 54.0, 21.0))
@@ -30,21 +32,16 @@ class ModernFeedbackViewController: UIViewController,UIPopoverPresentationContro
         inputLabel.userInteractionEnabled = true // this is required to allow the guesture recognizer to work
         super.init(coder: aDecoder)
     }
-
-    // MARK:- ViewController LifeCycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    func setupUIElements() {
         // crete the inputTapGesture for the inputLabel and then put the label on screen
         let inputTapGestuer:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "resetInputSlider")
         inputLabel.addGestureRecognizer(inputTapGestuer)
-        self.view.addSubview(inputLabel)
         
         // Setup the sliders for input and output.
         inputSlider.maximumValue = 10
         inputSlider.minimumValue = -10
         inputSlider.value = 0
-        view.addSubview(inputSlider)
         // This allows the programmatic constraints to be the only constrains :)
         inputSlider.setTranslatesAutoresizingMaskIntoConstraints(false)
         // Finally connect the input slider to the inputChanged action!
@@ -56,32 +53,27 @@ class ModernFeedbackViewController: UIViewController,UIPopoverPresentationContro
         outputSlider.value = 0
         outputSlider.enabled = false
         outputSlider.tintColor = UIColor.lightGrayColor() // trying a light gray for the tracking
-        view.addSubview(outputSlider)
         outputSlider.setTranslatesAutoresizingMaskIntoConstraints(false)
         //outputSlider.thumbTintColor = UIColor.grayColor()
         
         infoButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+    }
+
+    // MARK:- ViewController LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        // Ok, now we do some magic to make these bad boys look right... and this is before the logic to make the
-        // feedback model get laid out... This "Modern" method seems a lot more cluttered than the old resizing
-        // masks ever did. Oh well... We must do what we must do.
-        view.addConstraint(NSLayoutConstraint(item: inputSlider, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Height, multiplier: 0.90, constant: 0.0))
-        let constant:CGFloat = -1.0*((0.90*view.bounds.size.height/2)-(inputSlider.intrinsicContentSize().height/2))
-        NSLog("height: %f", constant);
-        view.addConstraint(NSLayoutConstraint(item: inputSlider, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Left, multiplier:1.0, constant: constant))
-        view.addConstraint(NSLayoutConstraint(item: inputSlider, attribute:NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0.0))
+        // setup the UIElements to be used
+        setupUIElements()
         
-        // Now we need to fix the output slider. Again, we use the same principles as above.
-        view.addConstraint(NSLayoutConstraint(item: outputSlider, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Height, multiplier: 0.90, constant: 0.0))
-        view.addConstraint(NSLayoutConstraint(item: outputSlider, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: -1.0*constant))
-        view.addConstraint(NSLayoutConstraint(item: outputSlider, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0.0))
-        
-        // Now position the infoButton relative to the slider. Due to the height of the slider really being it's width, we want half this value which is actually the center in the X axis (by auto-layout) so that's used to position the button on top of the slider just off center. Then by using the height (the fixed height of a normal UISlider) the button can be adjusted to sit nicely at the side.
-        view.addConstraint(NSLayoutConstraint(item: infoButton, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: outputSlider, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: -0.75*outputSlider.bounds.height))
-        
-        view.addConstraint(NSLayoutConstraint(item: selectModelButton, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: inputSlider, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0.75*inputSlider.bounds.height))
-        
+        // Initalise a layout controller to handle laying out the class
         let layoutController = LayoutFeedbackView(aView: view, aModel: sysModel)
+        
+        // Layout the basics:
+        layoutController.layoutIOSliders(inputSlider: inputSlider, outputSlider: outputSlider)
+        layoutController.layoutInputLabel(inputLabel)
+        layoutController.layoutInfoButton(infoButton, rightOf: outputSlider)
+        layoutController.layoutSelectModelButton(selectModelButton, leftOf: inputSlider)
     }
     
     override func viewDidAppear(animated: Bool) {
