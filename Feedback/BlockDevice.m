681b5dba7e22aa2fd7a8aa22854ed196275b7f5e
Ollie Hayman <discorev@olliespage.net>
2015-05-23 15:47:06 +0100

    Updated copyright headers to reflect the GPLv2 license

--- Feedback/BlockDevice.m
+++ Feedback/BlockDevice.m
@@ -4,7 +4,20 @@
 //
 //  Created by Oliver Hayman on 16/07/2012.
 //  Updated by Oliver Hayman on 01/08/2014.
-//  Copyright (c) 2014 OlliesPage. All rights reserved.
+//  Copyright (c) 2014 OlliesPage.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import "BlockDevice.h"
